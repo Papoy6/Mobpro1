a693fcd010d4afbd9b3bfa73428e2505bbadc452
Adam Zakky Maulana <adamzaky05@gmail.com>
2026-03-04 16:39:09 +0700

    Modul 02

--- app/src/main/java/com/adam0006/mobpro1/MainActivity.kt
+++ app/src/main/java/com/adam0006/mobpro1/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.adam0006.mobpro1
 
-import android.content.pm.ConfigurationInfo
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,9 +19,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.adam0006.mobpro1.ui.theme.Mobpro1Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +37,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Mobpro1Theme {
-             MainScreen()
+                MainScreen()
             }
         }
     }
@@ -65,17 +77,50 @@ fun MainScreen() {
             )
         }
     ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
+        ScreenContent(
+            modifier = Modifier.padding(innerPadding),
+            text = { content, style -> Text(text = content, style = style) },
+        )
 
     }
 }
 
 @Composable
-fun ScreenContent(modifer: Modifier = Modifier) {
-    Text(
-        text = "Hello Andriod!",
-        modifier = modifer
-    )
+fun ScreenContent(modifier: Modifier = Modifier, text: @Composable (String, TextStyle) -> Unit) {
+    var number by remember { mutableStateOf(0) }
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        text(
+            number.toString(),
+            MaterialTheme.typography.displayLarge,
+        )
+        Button(
+            onClick = {number++},
+            modifier = Modifier.fillMaxWidth(0.5f).padding(top = 16.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            text(stringResource(R.string.count), MaterialTheme.typography.labelLarge)
+        }
+    }
 }
 
-annotation class Modifer
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ScreenContentPreview() {
+    Mobpro1Theme {
+        ScreenContent(text = { content, style -> Text(text = content, style = style) })
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Mobpro1Theme {
+        Greeting("Android")
+    }
+}
