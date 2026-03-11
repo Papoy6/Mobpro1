e8b814afc09edccc9ff513ae4d62077ac4792006
Adam Zakky Maulana <adamzaky05@gmail.com>
2026-03-11 09:35:25 +0700

    Modul 3

--- app/src/main/java/com/adam0006/mobpro1/MainActivity.kt
+++ app/src/main/java/com/adam0006/mobpro1/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.adam0006.mobpro1
 
-import android.content.pm.ConfigurationInfo
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,9 +21,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adam0006.mobpro1.model.Hewan
 import com.adam0006.mobpro1.ui.theme.Mobpro1Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +40,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Mobpro1Theme {
-             MainScreen()
+                MainScreen()
             }
         }
     }
@@ -39,7 +54,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
@@ -52,6 +66,14 @@ fun MainScreenPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val data = listOf(
+        Hewan("Ayam", R.drawable.ayam),
+        Hewan("Bebek", R.drawable.bebek),
+        Hewan("Domba", R.drawable.domba),
+        Hewan("Kambing", R.drawable.kambing),
+        Hewan("Sapi", R.drawable.sapi),
+    )
+    var index by remember { mutableIntStateOf(0) }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -65,17 +87,46 @@ fun MainScreen() {
             )
         }
     ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
-
+        ScreenContent(
+            hewan = data[index % data.size],
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            index++
+        }
     }
 }
 
 @Composable
-fun ScreenContent(modifer: Modifier = Modifier) {
-    Text(
-        text = "Hello Andriod!",
-        modifier = modifer
-    )
+fun ScreenContent(
+    hewan: Hewan,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = hewan.imageResId),
+            contentDescription = stringResource(R.string.gambar, hewan.nama),
+            modifier = Modifier.size(132.dp)
+        )
+        Text(
+            text = hewan.nama,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(top = 24.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            Text(text = stringResource(R.string.lanjut))
+        }
+    }
 }
-
-annotation class Modifer
---
+++ app/src/main/java/com/adam0006/mobpro1/model/Hewan.kt
@@ -0,0 +1,6 @@
+package com.adam0006.mobpro1.model
+
+data class Hewan(
+    val nama: String,
+    val imageResId: Int
+)
