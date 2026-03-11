0c380812c1f22e179a989eb280f78ad6fc482f77
Adam Zakky Maulana <adamzaky05@gmail.com>
2026-03-11 09:54:08 +0700

    Challenge 3

--- app/src/main/java/com/adam0006/mobpro1/MainActivity.kt
+++ app/src/main/java/com/adam0006/mobpro1/MainActivity.kt
@@ -19,19 +19,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.adam0006.mobpro1.model.Hewan
 import com.adam0006.mobpro1.ui.theme.Mobpro1Theme
 
 class MainActivity : ComponentActivity() {
@@ -46,14 +45,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
@@ -66,67 +57,53 @@ fun MainScreenPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
-    val data = listOf(
-        Hewan("Ayam", R.drawable.ayam),
-        Hewan("Bebek", R.drawable.bebek),
-        Hewan("Domba", R.drawable.domba),
-        Hewan("Kambing", R.drawable.kambing),
-        Hewan("Sapi", R.drawable.sapi),
-    )
-    var index by remember { mutableIntStateOf(0) }
+    var isLampOn by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
+                title = { Text(text = stringResource(id = R.string.app_name)) }
             )
         }
     ) { innerPadding ->
         ScreenContent(
-            hewan = data[index % data.size],
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            index++
-        }
+            isLampOn = isLampOn,
+            modifier = Modifier.padding(innerPadding),
+            onToggleLamp = { isLampOn = !isLampOn } // Higher-Order Function [cite: 246, 362]
+        )
     }
 }
 
 @Composable
-fun ScreenContent(
-    hewan: Hewan,
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit
-) {
+fun ScreenContent(isLampOn: Boolean, modifier: Modifier = Modifier, onToggleLamp: () -> Unit) {
     Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        modifier = modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        val imageRes = if (isLampOn) R.drawable.lampu_nyala else R.drawable.lampu_mati
+        val statusText = if (isLampOn) R.string.lampu_hidup else R.string.lampu_mati
+        val buttonText = if (isLampOn) R.string.matikan else R.string.hidupkan
+
         Image(
-            painter = painterResource(id = hewan.imageResId),
-            contentDescription = stringResource(R.string.gambar, hewan.nama),
-            modifier = Modifier.size(132.dp)
+            painter = painterResource(id = imageRes),
+            contentDescription = stringResource(R.string.desc_lampu),
+            modifier = Modifier.size(200.dp), // Ukuran bebas [cite: 196]
+            contentScale = ContentScale.Fit
         )
+
         Text(
-            text = hewan.nama,
+            text = stringResource(id = statusText),
             style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier.padding(top = 16.dp)
         )
+
         Button(
-            onClick = onClick,
-            modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(top = 24.dp),
-            contentPadding = PaddingValues(16.dp)
+            onClick = onToggleLamp,
+            modifier = Modifier.fillMaxWidth(0.5f).padding(top = 24.dp),
+        contentPadding = PaddingValues(16.dp)
         ) {
-            Text(text = stringResource(R.string.lanjut))
-        }
+        Text(text = stringResource(id = buttonText))
+    }
     }
 }
