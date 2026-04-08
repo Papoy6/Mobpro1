c80af1a2bbbe352cb77e8b1826bba536a066a2b4
Adam Zakky Maulana <adamzaky05@gmail.com>
2026-04-08 15:06:44 +0700

    Challenge 5

--- app/src/main/java/com/adam0006/mobpro1/navigation/NavGraph.kt
+++ app/src/main/java/com/adam0006/mobpro1/navigation/NavGraph.kt
@@ -5,7 +5,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.adam0006.mobpro1.ui.screen.MainScreen
-import com.adam0006.mobpro1.navigation.AboutScreen
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
